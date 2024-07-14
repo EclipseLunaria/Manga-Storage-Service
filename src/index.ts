5f2b10f38d77d64f18f02c6419af1ab1f2ecc595
Jackson Dorsett <dorsettj@gmail.com>
2024-07-14 09:04:36 -0700

    dot env dependency and updated server listen log

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
 import express from "express";
 import process from "process";
-import dotenv from "dotenv"
 import router from "./routes";
 
-const app = express()
+const app = express();
 // load environment variable or default to 6968
-const port = process.env.PORT || 6968
-app.use("/storage/", router)
+const port = process.env.PORT || 6966;
+app.use("/storage/", router);
 
 app.get("/", (req, res) => {
-    res.send("Hello World!")
+  res.send("Storage Service is online.");
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-})
+  console.log(
+    `Storage Service is running locally to http://localhost:${port} and internally as http://storage-service:${port}`
+  );
+});
