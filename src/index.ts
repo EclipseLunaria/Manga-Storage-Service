0a22bf860f4aba7a585ebced6508bb2818796dcc
Jackson Dorsett <dorsettj@gmail.com>
2024-07-16 12:07:04 -0700

    added common router

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import express from "express";
 import process from "process";
-import router from "./routes";
+import router from "./routes/routes";
+import commonRouter from "./routes/shared";
 
 const app = express();
 // load environment variable or default to 6968
 const port = process.env.PORT || 6966;
+
+//attach common router
+app.use("/common/", commonRouter);
+
+//attach storage router
 app.use("/storage/", router);
 
 app.get("/", (req, res) => {
